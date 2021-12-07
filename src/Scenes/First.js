f1c94e9b8bb92919f6160cd321d2dc5d44535f8f
Hadi Golkarian <HadiGolkarian@gmail.com>
2021-12-07 12:40:41 +0330

    fixed copy

--- src/Scenes/First.js
+++ src/Scenes/First.js
@@ -29,7 +29,30 @@ let INTERSECTED;
 let intersected;
 
 class CityEnvironment {
+  copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+      // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+      return window.clipboardData.setData("Text", text);
 
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+      var textarea = document.createElement("textarea");
+      textarea.textContent = text;
+      textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+      }
+      catch (ex) {
+        console.warn("Copy to clipboard failed.", ex);
+        return false;
+      }
+      finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  }
   constructor() {
 
     this.container = window;
@@ -120,7 +143,7 @@ class CityEnvironment {
       },
 
     };
-    navigator.clipboard.writeText(JSON.stringify(data));
+    this.copyToClipboard(JSON.stringify(data));
   }
   setupCamera() {
     this.camera.position.set(-22, 25, 20);
